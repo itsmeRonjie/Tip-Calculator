efc6cb9bbc5e3d01709313f1a92f1f0a5697a90e
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-21 11:14:22 +0800

    - added TestLocalizationView

---
+++ Tip-Calculator/View/Test View/TestLocalizationView.swift
@@ -0,0 +1,29 @@
+//
+//  TestLocalizationView.swift
+//  Tip-Calculator
+//
+//  Created by Ronjie Diafante Man-on on 5/21/25.
+//
+
+import SwiftUI
+
+struct TestLocalizationView: View {
+    var body: some View {
+        VStack {
+            Text("Amount")
+            Text("Bill")
+            Text("Choose Tip")
+            Text("Custom")
+            Text("Enter Total")
+            Text("Split")
+            Text("Summary")
+            Text("Tip")
+            Text("Tip Calculator")
+            Text("Total per person")
+        }
+    }
+}
+
+#Preview {
+    TestLocalizationView()
+}
